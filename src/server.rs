9e2e75b4e7ec0aa11597ee939de5c92b1c278a3d
loxp <loxppqqxol@gmail.com>
2019-09-19 00:04:41 +0800

    remove unwrap

--- src/server.rs
+++ src/server.rs
@@ -10,10 +10,9 @@ pub struct Server {
 impl Server {
     pub fn new(path: &str) -> KvdResult<Server> {
         let mut settings = config::Config::default();
-        settings.merge(config::File::with_name(path)).unwrap();
+        settings.merge(config::File::with_name(path))?;
 
         let wal_dir = settings.get_str("wal_dir")?;
-
         let store = Store::open(PathBuf::from(wal_dir))?;
         let server = Server { store };
 
